b7f4023792e8997c5b0c522175561076499fb1f9
jdgarza <jdgarza@uchicago.edu>
2016-01-13 09:09:14 -0600

    solution_8

---
+++ solution_8.py
@@ -0,0 +1,10 @@
+#hw1
+#   A program to temperatures from Fahrenheit to Celsius
+#by: Jessica Garza
+
+def main():
+  fahrenheit = eval(input("What is the Fahrenheit temperature? "))
+  celsius = 5/9 * (fahrenheit - 32)
+  print("The temperature is", celsius, "degrees Celsius.")
+
+main()
